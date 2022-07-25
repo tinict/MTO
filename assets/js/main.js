adbf3dc9689222bf1dcd08b45e262d68a2f0e5ac
Tín Nguyễn <89054327+tinict@users.noreply.github.com>
2022-07-25 09:00:13 +0700

    Test-08

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,8 +39,138 @@ for(var i = 0; i < btnElement.length; ++i){
             for(var j = 0; j < elemment.length; ++j){
                 elemment[j].style.display = 'none';
             }
-            document.querySelector(".order__mor").style.display = 'block';
+            document.querySelector(".order__Food").style.display = 'block';
         }
-        console.log(document.querySelector(".order__Fd"));
+        else if(e.target.innerHTML === "All"){
+            var elemment = document.querySelectorAll(".box");
+            for(var j = 0; j < elemment.length; ++j){
+                elemment[j].style.display = 'none';
+            }
+            for(var j = 0; j < elemment.length; ++j){
+                elemment[j].style.display = 'block';
+            }
+        }
+        console.log(document.querySelector(".order__Food").style.display);
     }
 }
+
+function getNumberInString(str){
+    var temp = "";
+    for(var i = str.indexOf("-") + 1; i < str.length; ++i){
+        temp += str[i];
+    }
+    return Number(temp);
+}
+
+var plusElement = document.querySelectorAll(".fa-square-plus");
+var minusElement = document.querySelectorAll(".fa-square-minus");
+var numElement = document.querySelectorAll(".count")
+
+function NoneDisplay(Count,index){
+    if(Count <= 0){
+        minusElement[index].style.display = "none";
+        numElement[index].style.display = "none";
+    }
+}
+
+for(var i = 0; i < minusElement.length; ++i){
+    minusElement[i].style.display = "none";
+    numElement[i].style.display = "none";
+}
+
+for(var i = 0; i < plusElement.length; ++i){
+    plusElement[i].onclick = function(e){
+        var temp = e.target.id;
+        var index = getNumberInString(temp);
+        var num = Number(numElement[index-1].textContent);
+        numElement[index-1].textContent = String(++num);
+        minusElement[index-1].style.display = "block";
+        numElement[index-1].style.display = "block";
+        console.log(index);
+        console.log(Number(numElement[index-1].textContent));
+    }
+    minusElement[i].onclick = function(e){
+        var temp = e.target.id;
+        var index = getNumberInString(temp);
+        var num = Number(numElement[index-1].textContent);
+        NoneDisplay(--num,index-1);
+        if(num <= 0){
+            num = 1;
+        }
+        numElement[index-1].textContent = String(--num);
+        console.log(index);
+        console.log(Number(numElement[index-1].textContent));
+    }
+}
+
+// for(var i = 0; i < plusElement.length; ++i){
+//     plusElement[i].onclick = function(e){
+//         numElement[i]. = function(e){
+//            e.target.innerHTML = "1";
+//         }
+//     }
+// }
+
+// var Products = [
+//     {
+        // id: 1,
+        // name: 'coffee',
+        // category: 'cf',
+        // cost: 20000,
+//     },
+//     {
+//         id: 2,
+//         name: 'coffee',
+//         category: 'cf',
+//         cost: 20000,
+//     }
+// ];
+
+// class Product{
+//     constructor(id,name,category,cost){
+//         this.id = id;
+//         this.name = name;
+//         this.category = category;
+//         this.cost = cost;
+//     }
+//     getID(){
+//         return this.id;
+//     }
+// };
+
+// var P = new Product("1","hi","d","2000");
+
+// const Products = new Array();
+// Products.push(P);
+
+// console.log(Products[0].getID());
+
+// console.log(Products[0]);
+// var listProductBlock = document.querySelector('.order__box');
+// var htmls = Products.map(function(){
+//     return `
+//         <div class="box__item">
+//             <img src="assets/img/Business-Coffee-opened-1.png" alt="">
+//             <div class="box__contents">
+//                 <span class = "id-Product" style = "display:none;">${Products[0].getID()}</span>
+//                 <span class = "name-Product">${Products[0].getID()}</span>
+//                 <div class="content__container">
+//                     <div class="order__cost">
+//                         <span class="money-item">20,000đ</span>
+//                         <div class="groupButton">
+//                             <i class="fa-solid fa-square-minus btn-hint"></i>
+//                             <span class="count btn-hint">0</span>
+//                             <i class="fa-solid fa-square-plus"></i>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     `;
+// });
+
+// listProductBlock.innerHTML = htmls.join('');
+
+// var plus = document.querySelector('.id-Product');
+// console.log(plus.innerHTML)
+
